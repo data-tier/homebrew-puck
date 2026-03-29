8142fbcb1a02beb47fae1650454c97edb16dfe0e
Maheshwar Mudi <5460175+mudim@users.noreply.github.com>
2026-03-29 16:03:10 +0100

    fix: point download URLs to homebrew-puck releases

--- Formula/puck-agent.rb
+++ Formula/puck-agent.rb
@@ -6,22 +6,22 @@ class PuckAgent < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/data-tier/puck-agent/releases/download/v#{version}/puck-agent-darwin-arm64"
+      url "https://github.com/data-tier/homebrew-puck/releases/download/v#{version}/puck-agent-darwin-arm64"
       sha256 "b78f0f59af385daf0d7a3da92cc2becb566f390cf15962c652886294fd1b4059"
     end
     on_intel do
-      url "https://github.com/data-tier/puck-agent/releases/download/v#{version}/puck-agent-darwin-amd64"
+      url "https://github.com/data-tier/homebrew-puck/releases/download/v#{version}/puck-agent-darwin-amd64"
       sha256 "c4e72570b519bf86f598015eb75d3982105a550fee86b372d6e97f2cc2e6395a"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/data-tier/puck-agent/releases/download/v#{version}/puck-agent-linux-arm64"
+      url "https://github.com/data-tier/homebrew-puck/releases/download/v#{version}/puck-agent-linux-arm64"
       sha256 "1fb25bd25316ce3fc7714fdb156e7c8fb24bee6527073949f700114965aefcc7"
     end
     on_intel do
-      url "https://github.com/data-tier/puck-agent/releases/download/v#{version}/puck-agent-linux-amd64"
+      url "https://github.com/data-tier/homebrew-puck/releases/download/v#{version}/puck-agent-linux-amd64"
       sha256 "2ba81d0eda110efc635cacfafbfdbaffadf56fa25db80ba63c50d4da5bebe1c8"
     end
   end
